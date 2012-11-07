885072e436bdb6ed415d6d485803f31df7d30a64
Caroline Amado <x66girl@gmail.com>
2012-11-07 20:56:47 -0200

    I tranlated the comments for english, but I need to change the variables name

--- Filter.m
+++ Filter.m
@@ -1,42 +1,42 @@
-%FIR COM FASE LINEAR - EQUAÇÃO T4 - BANDPASS
+%Digital Filter FIR with linear phase - EQUATION T4 - BANDPASS
 
 clear all
 close all
 
-%Ordem do filtro (Ímpar, valor dado)
+%Order of the filter (Odd, value given)
 L = 31; 
 
-%Frequência de corte inferior (Valor Dado)
+%Low-frequency cutoff (Value Given)
 fcl = 7e3;
 
-%Frequência de corte superior (Valor Dado)
+%Upper cut-off frequency (Given Value)
 fc2 = 28e3;
 
-%Frequência de amostragem (Valor Dado)
+%Sampling frequency (Given Value)
 Fs = 70e3;
 
-%Frequência de corte normalizadas, pois a função firls exige isso
+%Cutoff frequency standard because it requires by the function firls
 freq2 = 2*fc2/Fs;
 freq1 = 2*fcl/Fs;
 
-%Vetor com as frequências de corte
+%Vector with cutoff frequencies
 freq = [0 freq1/2 freq1 freq2 (1-freq1/2) 1];
 
-%Vetor Amplitudes para interpolarização, requerido pela função do matlab
+%Vector Amplitudes to interpolate, required by the matlab function
 amp = [0 0 1 1 0 0];
 
 
-% Calculo dos coeficientes de h
+%Calculation of coefficients h
 h = firls(L,freq,amp,'h');
 
-%Grafico dos coeficientes de h discretos
+%Graph discrete coefficients h
 figure(1)
 subplot(2,1,1)
 stem(0:L,h)
-title('Coeficientes (simetria impar)');
-xlabel('índice n'); ylabel('valor do coeficiente');
+title('Ratios (odd symmetry)');
+xlabel('index n'); ylabel('coefficient');
 
-%Intervalo omega discreto que serão os pontos utilizado no gráfico
+%Interval omega discrete points that will be used in the graph
 omegadis = 0:0.01:pi;
 for l = 1:length(omegadis)
    for n= 0:(L-1)/2
@@ -48,16 +48,16 @@ end
 
 figure(3)
 plot(omegadis/pi,abs(H),'-b');grid minor
-title('Resposta de Frequência');
-xlabel('Omega normalizado'); ylabel('Magnitude');
+title('Frequency Response');
+xlabel('Omega standard'); ylabel('Magnitude');
 
-%Quantização
+%Quantization
 
-%Criação de uma variavel chamada htemp para se manipular os coeficientes do
-%filtro
+%Creating a variable called htemp to manipulate the filter coefficients
+
 htemp = h;
 
-%Parte em que é determinado o bit de sinal e armazenado em hsinal
+%Part where is determined that the sign bit and stored in hsinal
 for m=1:length(htemp)
     
  
@@ -72,7 +72,7 @@ for m=1:length(htemp)
         hsinal(m) = 0;
     end  
    
-     % trasnformação em binário
+     % Converting in binary
     for s = 2:8
             htemp(m) = htemp(m)*2;
             
@@ -111,11 +111,11 @@ end
 figure(1)
 subplot(2,1,2)
 stem(0:L,hquan)
-title('Coeficientes Quantizados');
-xlabel('índice n'); ylabel('valor do coeficiente');
+title('Quantized coefficients');
+xlabel('index n'); ylabel('cofficient');
 
 
-% Calculo e plot de H3 com coeficientes de 8 bits equação 8.12
+% Calculate and plot H3 8-bit coefficients with equation 8.12
 for l = 1:length(omegadis)
     for n = 0:(L-1)/2
         valores(n+1) = 2*hquan(n+1)*sin(omegadis(l)*(n - (L/2)));
@@ -126,18 +126,18 @@ end
 
 figure(3)
 plot(omegadis/pi,abs(Hquan),'-b');grid minor
-title('Resposta de Frequência Quantizado');
-xlabel('Omega normalizado'); ylabel('Magnitude');
+title('Frequency Response quantized');
+xlabel('Omega standard'); ylabel('Magnitude');
 
-%Gráficos
+%Grafics
 figure
 subplot(2,1,1)
 plot(omegadis/pi,abs(H),'-b');grid minor
 hold
 
 plot(omegadis/pi,abs(Hquan),':r'); grid minor
-title('Módulo de H (azul- Módulo de Função H e vermelho- H quantizado)');
-xlabel('Omega normalizado'); ylabel('Magnitude');
+title('Module H (blue-Function Module H and H red-quantized)');
+xlabel('Omega standard'); ylabel('Magnitude');
 
 subplot(2,1,2)
 plot(omegadis/pi,angle(H),'-b');grid minor
@@ -145,6 +145,6 @@ hold
 
 plot(omegadis/pi,angle(Hquan),':r'); grid minor
 title('Fase de H');
-xlabel('Omega normalizado'); ylabel('Fase');
+xlabel('Omega standard'); ylabel('Phase');
 
 
